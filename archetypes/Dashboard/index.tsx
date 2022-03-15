97603a3ceda0c5a144a37431c14bf40c953577fd
sethfgn <sethfgn@gmail.com>
2022-03-15 10:46:23 -0400

    Cleaner container formatting

--- archetypes/Dashboard/index.tsx
+++ archetypes/Dashboard/index.tsx
@@ -1,17 +1,32 @@
 import Container from '@components/General/Container';
 
+//mt-12
 export default (() => {
     return (
-        <div className="container mt-12 flex absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            {['Total Value Locked', 'Total National Locked', 'All Time Value']
-                .map((e) => <Container>{e}</Container>)
-                .reduce((prev, curr) => (
-                    <>
-                        {prev}
-                        <div className='m-6' />
-                        {curr}
-                    </>
-                ))}
+        <div className="flex flex-col">
+            <div className="flex flex-row mb-10 container">
+                {['Total Value Locked', 'Total National Locked', 'All Time Value']
+                    .map((e) => (
+                        <div className="box h-32 w-1/3 p-5 mt-12">
+                            <Container>
+                                {e}
+                                <div className="p-20" />
+                            </Container>
+                        </div>
+                    ))
+                    .reduce((prev, curr) => (
+                        <>
+                            {prev}
+                            <div className="m-6" />
+                            {curr}
+                        </>
+                    ))}
+            </div>
+            <div className="flex flex-row mt-10">
+                <div className="h-32 p-5 container mt-12">
+                    <Container>Hi</Container>
+                </div>
+            </div>
         </div>
     );
 }) as React.FC<any>;
