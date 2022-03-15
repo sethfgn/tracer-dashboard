f0488b96520139a8b186562b5f692a851b044d03
jwstanly <jwstanly@yahoo.com>
2022-03-15 09:51:50 -0400

    typing correction

--- libs/utils/totalValueLockedAPI.ts
+++ libs/utils/totalValueLockedAPI.ts
@@ -3,11 +3,13 @@ import * as dummy_data from './example_tvl_data.json';
 const BASE_TVL_API = process.env.NEXT_PUBLIC_BASE_TVL_API;
 
 export type TvlDataPoint = {
-    time_stamp: number;
+    time_stamp: string; // unix timestamp (seconds)
     tvl: number;
 };
 
-export type TvlSeries = TvlDataPoint[];
+export type TvlSeries = {
+    [ethAddress: string]: TvlDataPoint[];
+};
 
 export const fetchTvlSeries: () => Promise<TvlSeries> = async () => {
     const route = `${BASE_TVL_API}/unknown/path`;
