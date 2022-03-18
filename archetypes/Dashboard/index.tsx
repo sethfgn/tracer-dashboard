08d857776a94201be526c77b397b8566c4c0908d
jwstanly <jwstanly@yahoo.com>
2022-03-18 11:16:00 -0400

    chart testing

--- archetypes/Dashboard/index.tsx
+++ archetypes/Dashboard/index.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 import Button from '@components/General/Button';
 import Container from '@components/General/Container';
 import GreyContainer from '@components/GreyContainer';
+import PriceLineChart from '@components/Charts/PriceLineChart/index';
+import { fetchTvlSeries, TvlSeries } from '../../libs/utils/totalValueLockedAPI';
+import { LineData } from '../../components/Charts/PriceLineChart/TracerTypes';
 
 //mt-12
 //"mt-5 flex overflow-x-auto whitespace-nowrap"
 export default (() => {
+    const [lineData, setLineData] = React.useState<LineData>();
+
+    async function getLineData() {
+        const tvlSeries = await fetchTvlSeries();
+        const newLineData: LineData = tvlSeries[Object.keys(tvlSeries)[0]].map((tvlPoint) => {
+            return {
+                time: tvlPoint.time_stamp,
+                value: tvlPoint.tvl,
+            };
+        });
+        setLineData(newLineData.splice(0, 10));
+    }
+
+    React.useEffect(() => {
+        getLineData();
+    }, []);
+
     return (
         // <div className="relative flex w-full py-2 text-center bg-theme-background-nav-secondary matrix:bg-transparent">
         <div className="mr-5 container">
@@ -47,6 +67,7 @@ export default (() => {
                     <div className="h-32 p-5 container mt-12">
                         <Container>
                             <div className="p-24">Cumulative Volume Changes</div>
+                            {/* <PriceLineChart lineData={lineData} /> */}
                         </Container>
                         <GreyContainer>Hello JW</GreyContainer>
                     </div>
