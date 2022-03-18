10743e3061653a390351b3cc7cd71fbe2ba81cbf
jwstanly <jwstanly@yahoo.com>
2022-03-18 10:17:43 -0400

    add key

--- archetypes/Dashboard/index.tsx
+++ archetypes/Dashboard/index.tsx
@@ -1,58 +1,49 @@
 import React from 'react';
-import Button from '@components/General/Button';
 import Container from '@components/General/Container';
-import GreyContainer from '@components/GreyContainer';
+import Button from '@components/General/Button';
 
 //mt-12
-//"mt-5 flex overflow-x-auto whitespace-nowrap"
 export default (() => {
     return (
-        // <div className="relative flex w-full py-2 text-center bg-theme-background-nav-secondary matrix:bg-transparent">
-        <div className="mr-5 container">
-            <div className="flex flex-col">
-                <div className="mt-5 flex overflow-x-auto whitespace-nowrap">
-                    <div className="mr-5 flex items-center">
-                        <div className="whitespace-nowrap shrink-0">Selected Market</div>
-                    </div>
-                    <div className="mr-5">
-                        <Button size="default" variant="primary">
-                            Short 3xBTC
-                        </Button>
-                    </div>
-                    <div>
-                        <Button size="default" variant="unselected">
-                            View All
-                        </Button>
-                    </div>
+        <div className="flex flex-col container">
+            <div className="mt-5 flex overflow-x-auto whitespace-nowrap">
+                <div className="mr-5 flex items-center">
+                    <div className="whitespace-nowrap shrink-0">Selected Market</div>
                 </div>
-                <div className="flex flex-row mb-10">
-                    {['Total Value Locked', 'Total National Locked', 'All Time Value']
-                        .map((e) => (
-                            <div key={e} className="box h-32 w-1/3 p-5 mt-12">
-                                <Container>
-                                    {e}
-                                    <div className="p-24" />
-                                </Container>
-                            </div>
-                        ))
-                        .reduce((prev, curr) => (
-                            <>
-                                {prev}
-                                <div className="m-6" />
-                                {curr}
-                            </>
-                        ))}
+                <div className="mr-5">
+                    <Button size="default" variant="primary">
+                        Short 3xBTC
+                    </Button>
                 </div>
-                <div className="flex flex-row mt-10">
-                    <div className="h-32 p-5 container mt-12">
-                        <Container>
-                            <div className="p-24">Cumulative Volume Changes</div>
-                        </Container>
-                        <GreyContainer>Hello JW</GreyContainer>
-                    </div>
+                <div>
+                    <Button size="default" variant="unselected">
+                        View All
+                    </Button>
+                </div>
+            </div>
+            <div className="flex flex-row mb-10">
+                {['Total Value Locked', 'Total National Locked', 'All Time Value']
+                    .map((e) => (
+                        <div key={e} className="box h-32 w-1/3 p-5 mt-12">
+                            <Container>
+                                {e}
+                                <div className="p-20" />
+                            </Container>
+                        </div>
+                    ))
+                    .reduce((prev, curr) => (
+                        <>
+                            {prev}
+                            <div className="m-6" />
+                            {curr}
+                        </>
+                    ))}
+            </div>
+            <div className="flex flex-row mt-10">
+                <div className="h-32 p-5 container mt-12">
+                    <Container>Hi</Container>
                 </div>
             </div>
         </div>
-        //</div>
     );
 }) as React.FC<any>;
