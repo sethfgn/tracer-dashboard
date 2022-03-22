3f60d4893224432d1dd5aed856acc0a3ae585437
jwstanly <jwstanly@yahoo.com>
2022-03-22 14:15:54 -0500

    dashboard visual changes

--- archetypes/Dashboard/index.tsx
+++ archetypes/Dashboard/index.tsx
@@ -5,7 +5,8 @@ import { fetchTvlSeries, TvlDataPoint } from '../../libs/utils/totalValueLockedA
 // import ChartWrapper from '@components/Charts';
 import { AxisOptions, Chart } from 'react-charts';
 import { Button, Card } from '@tracer-protocol/tracer-ui';
-import { classNames } from '../../libs/utils/functions';
+import { Dropdown } from '@components/General/Dropdown';
+import ChartCard from '../../components/ChartCard/index';
 
 //mt-12
 //"mt-5 flex overflow-x-auto whitespace-nowrap"
@@ -44,67 +45,88 @@ export default (() => {
     return (
         <div className="container">
             {/* Header */}
-            <div className="mt-5 flex overflow-x-auto whitespace-nowrap">
-                <div className="mt-5 flex overflow-x-auto whitespace-nowrap">
-                    <div className="mr-5 flex items-center">
-                        <div className="whitespace-nowrap shrink-0">Selected Market</div>
-                    </div>
-                    <div className="mr-5">
-                        <Button size="medium" variant="action">
-                            Short 3xBTC
-                        </Button>
-                    </div>
-                    <div>
-                        <Button size="medium" variant="focus">
-                            View All
-                        </Button>
-                    </div>
+            <div className="m-5 mt-10 flex overflow-x-auto whitespace-nowrap">
+                <div className="mr-5 flex items-center">
+                    <div className="whitespace-nowrap shrink-0">Selected Market</div>
+                </div>
+                <div className="mr-5">
+                    <Button size="medium" variant="action">
+                        Short 3xBTC
+                    </Button>
+                </div>
+                <div>
+                    <Button size="medium" variant="focus">
+                        View All
+                    </Button>
                 </div>
             </div>
             {/* Mini dashboards */}
-            <div className="flex flex-row mb-10">
+            <div className="flex mt-12 mb-10 flex-col md:flex-row">
                 {['Total Value Locked', 'Total National Locked', 'All Time Value']
                     .map((e) => (
-                        <div key={e} className="box w-1/3 mt-12">
-                            <Card>
-                                {e}
-                                <div className="p-24" />
-                            </Card>
+                        <div key={e} className="box md:w-1/3">
+                            <ChartCard title={e} />
                         </div>
                     ))
                     .reduce((prev, curr) => (
                         <>
                             {prev}
-                            <div className="m-6" />
+                            <div className="m-2" />
                             {curr}
                         </>
                     ))}
             </div>
             {/* Big dashboard */}
             <Card>
-                <div className="w-96 flex-col mb-5 ml-5">
-                    <GreyContainer>Hello JW</GreyContainer>
-                    <GreyContainer>Hello JW</GreyContainer>
-                    <GreyContainer>Hello JW</GreyContainer>
+                <div className="flex">
+                    <div className="pb-2 font-semibold">Cumulative Volume Changes</div>
                 </div>
-                <div className="p-24">Cumulative Volume Changes</div>
-                {lineData ? (
-                    <Chart
-                        options={{
-                            data: [
-                                {
-                                    label: 'React Charts',
-                                    data: lineData,
-                                },
-                            ],
-                            primaryAxis,
-                            secondaryAxes,
-                        }}
-                    />
-                ) : (
-                    <div>Loading...</div>
-                )}
+
+                <div className="flex">
+                    <div className=" flex-initial flex-col">
+                        <GreyContainer>
+                            <div className="font-bold text-3xl">$400,000</div>
+                            <div className="flex items-center">
+                                <div className="w-2 h-2 rounded-full bg-red-400 mr-1" />
+                                <div className="font-bold text-base opacity-50">Total Mint</div>
+                            </div>
+                        </GreyContainer>
+                        <GreyContainer>
+                            <div className="font-bold text-3xl">$360,000</div>
+                            <div className="flex items-center">
+                                <div className="w-2 h-2 rounded-full bg-blue-400 mr-1" />
+                                <div className="font-bold text-base opacity-50">Total Burn</div>
+                            </div>
+                        </GreyContainer>
+                        <GreyContainer>
+                            <div className="font-bold text-3xl">$40,000</div>
+                            <div className="flex items-center">
+                                <div className="w-2 h-2 rounded-full bg-green-400 mr-1" />
+                                <div className="font-bold text-base opacity-50">Total Secondary Liquidity</div>
+                            </div>
+                        </GreyContainer>
+                    </div>
+                    <div className="flex-auto ml-5">
+                        {lineData ? (
+                            <Chart
+                                options={{
+                                    data: [
+                                        {
+                                            label: 'React Charts',
+                                            data: lineData,
+                                        },
+                                    ],
+                                    primaryAxis,
+                                    secondaryAxes,
+                                }}
+                            />
+                        ) : (
+                            <div>Loading...</div>
+                        )}
+                    </div>
+                </div>
             </Card>
+            <div className="mb-20" />
         </div>
     );
 }) as React.FC<any>;
--- components/GreyContainer/index.tsx
+++ components/GreyContainer/index.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
+
 interface GreyContainerProps {
     children: React.ReactNode;
 }
 
 const GreyContainer = (props: GreyContainerProps) => {
     return (
-        <div className="w-1/4 h-32 mt-5 px-5 pt-10 pb-5 rounded-xl bg-cool-gray-200 dark:bg-theme-background-secondary">
-            <div className="font-bold text-2xl opacity-50">{props.children}</div>
+        <div className="mt-5 px-5 pt-5 pb-5 rounded-xl bg-cool-gray-100 dark:bg-theme-background-secondary">
+            {props.children}
         </div>
     );
 };
